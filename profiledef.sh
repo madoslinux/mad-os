284d854fd9b05dd65acbe08bf55508eb1fa9649c
madkoding <madkoding@gmail.com>
2026-03-06 13:37:39 -0300

    perf: increase zstd compression from level 3 to 17

--- profiledef.sh
+++ profiledef.sh
@@ -14,8 +14,8 @@ bootmodes=('bios.syslinux'
             'uefi.systemd-boot')
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '3')
-bootstrap_tarball_compression=('zstd' '-c' '-T0' '-zstd-level=3')
+airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '17')
+bootstrap_tarball_compression=('zstd' '-c' '-T0' '-zstd-level=17')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/etc/profile.d/mados-welcome.sh"]="0:0:755"
