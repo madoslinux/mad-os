63446a7fe51429b9ee20bca683ff8588fa91e0fa
madKoding <madkoding@gmail.com>
2026-03-16 19:12:09 -0300

    fix: restore sda3 for root partition in mados-installer patch

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -133,7 +133,6 @@ if [[ -d "$INSTALLER_PYTHON_DIR/.git" ]]; then
     if [[ -f "$INSTALLER_PYTHON_DIR/installer/config_script.py" ]]; then
         echo "  Patching mados-installer BIOS GRUB install fix..."
         sed -i 's/grub-install --target=i386-pc --recheck \$disk/grub-install --target=i386-pc --recheck "\$disk"/' "$INSTALLER_PYTHON_DIR/installer/config_script.py"
-        sed -i 's|/dev/sda3|/dev/sda2|' "$INSTALLER_PYTHON_DIR/installer/config_script.py"
     fi
 else
     echo "Installing $INSTALLER_APP from GitHub..."
@@ -149,8 +148,8 @@ else
             echo "  Patching mados-installer BIOS GRUB install fix..."
             # Replace the BIOS grub-install line to use the disk variable properly
             sed -i 's/grub-install --target=i386-pc --recheck \$disk/grub-install --target=i386-pc --recheck "\$disk"/' "$INSTALLER_PYTHON_DIR/installer/config_script.py"
-            # Fix hardcoded /dev/sda3 to use sda2 (common root partition)
-            sed -i 's|/dev/sda3|/dev/sda2|' "$INSTALLER_PYTHON_DIR/installer/config_script.py"
+            # Restore sda3 (root is partition 3 in the installer layout)
+            sed -i 's|/dev/sda2|/dev/sda3|' "$INSTALLER_PYTHON_DIR/installer/config_script.py" 2>/dev/null || true
         fi
         
         # Create launcher script
